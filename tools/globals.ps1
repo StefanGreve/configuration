f2a090da4c576c18b317febab475e2d0fcc3c734
StefanGreve <greve.stefan@outlook.jp>
2023-10-03 21:14:42 +0200

    Migrate refactored globals.ps1 file from confiles repository

---
+++ tools/globals.ps1
@@ -0,0 +1,6 @@
+# General
+$global:DESKTOP = [Environment]::GetFolderPath("Desktop")
+$global:DOCUMENTS = [Environment]::GetFolderPath("MyDocuments")
+
+# Windows
+$global:HOSTS = "C:\Windows\System32\Drivers\etc\hosts"

04d28534739aa328bad301b15581cb85801bb05b
StefanGreve <greve.stefan@outlook.jp>
2023-10-03 15:36:26 +0200

    Convert installer script to Cmdlet
    * add mutually exclusive parameter groups to script
    * add flag for installing VS Code extensions and applications
    * add dynamic switch for installing registry files
    
    NOTE: the "reg import" command doesn't disturb the flow of the
    program with an interactive prompt, as opposed to Invoke-Item

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,33 +1,76 @@
 using namespace System.IO
+using namespace System.Management.Automation
 
-#region Initialization
+[CmdletBinding()]
+[OutputType([void])]
+param(
+    [Parameter(ParameterSetName = "Custom")]
+    [switch] $Applications,
 
-$Root = git rev-parse --show-toplevel
-. $([Path]::Combine($Root, "scripts", "utils.ps1"))
-$OperatingSystem = Get-OperatingSystem
-$Apps = Get-Content -Path $([Path]::Combine($Root, "settings", "apps.json")) -Raw | ConvertFrom-Json
+    [Parameter(ParameterSetName = "Custom")]
+    [switch] $Plugins,
 
-#endregion
+    [Parameter(ParameterSetName = "All", Mandatory)]
+    [switch] $All
+)
+dynamicparam {
+    if ([OperatingSystem]::IsWindows()) {
+        $ParamDictionary = New-Object -Type RuntimeDefinedParameterDictionary
+        $AttributeCollection = New-Object -Type System.Collections.ObjectModel.Collection[System.Attribute]
 
-Push-Location -Path $Root
+        $RegistryParameter = New-Object -Type RuntimeDefinedParameter("Registry", [switch], $AttributeCollection)
 
-#region Install Programs
+        $RegistryAttribute = New-Object ParameterAttribute
+        $RegistryAttribute.ParameterSetName = "Custom"
 
-$PackageManagers = $Apps | Select-Object -ExpandProperty $OperatingSystem
-
-switch ($OperatingSystem) {
-    "Windows" {
-        $WingetPackages = $PackageManagers.Winget
-        winget install --id $WingetPackages --accept-package-agreements --accept-source-agreements --disable-interactivity
-     }
-    "Linux" {
-        throw [NotImplementedException]::new("TODO")
-    }
-    "MacOS" {
-        throw [NotImplementedException]::new("TODO")
+        $AttributeCollection.Add($RegistryAttribute)
+        $ParamDictionary.Add("Registry", $RegistryParameter)
+        return $ParamDictionary
     }
 }
 
-#endregion
+begin {
+    $Root = git rev-parse --show-toplevel
+    . $([Path]::Combine($Root, "scripts", "utils.ps1"))
+    $OperatingSystem = Get-OperatingSystem
+    $Apps = Get-Content -Path $([Path]::Combine($Root, "settings", "apps.json")) -Raw | ConvertFrom-Json
+    Push-Location -Path $Root
+}
+process {
+    if ($Applications.IsPresent -or $All.IsPresent) {
+        $PackageManagers = $Apps | Select-Object -ExpandProperty $OperatingSystem
 
-Pop-Location
+        switch ($OperatingSystem) {
+            "Windows" {
+                $WingetPackages = $PackageManagers.Winget
+                winget install --id $WingetPackages --accept-package-agreements --accept-source-agreements --disable-interactivity
+             }
+            "Linux" {
+                throw [NotImplementedException]::new("TODO")
+            }
+            "MacOS" {
+                throw [NotImplementedException]::new("TODO")
+            }
+        }
+    }
+
+    if ($PSBoundParameters.Registry -or ([OperatingSystem]::IsWindows() -and $All.IsPresent)) {
+        $RegistryFiles = Get-ChildItem -Path $([Path]::Combine($Root, "settings")) -Filter *.reg
+        $RegistryFiles | ForEach-Object {
+            Write-Verbose $_.FullName
+            reg import $_.FullName
+        }
+    }
+
+    if ($Plugins.IsPresent -or $All.IsPresent) {
+        $Extensions = $Apps | Select-Object -ExpandProperty Extensions
+
+        $Extensions.Code | ForEach-Object -ThrottleLimit 5 -Parallel {
+            # force parameter updates to the latest version, if already installed
+            code --install-extension $_ --force
+        }
+    }
+}
+clean {
+    Pop-Location
+}

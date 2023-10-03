143cf765b156ba025e28bf19a84a9b194fceccee
StefanGreve <greve.stefan@outlook.jp>
2023-10-03 21:15:02 +0200

    Add another step: symlink powershell scripts to script folder
    * try to read the value specified in the PROFILE_LOAD_CUSTOM_SCRIPTS
      environment variable set by the default PowerShell profile, else
      revert to the folder located in MyDocuments/Scripts

--- scripts/link.ps1
+++ scripts/link.ps1
@@ -7,15 +7,16 @@ $Root = git rev-parse --show-toplevel
 $OperatingSystem = Get-OperatingSystem
 $Config = Get-Content -Path $([Path]::Combine($Root, "settings", "config.json")) -Raw | ConvertFrom-Json
 $Assets = [Path]::Combine($HOME, ".config", "assets")
-$Total = 2
+$Scripts = $env:PROFILE_LOAD_CUSTOM_SCRIPTS ?? [Path]::Combine($HOME, "Documents", "Scripts")
+$Total = 3
 
 #endregion
 
 Push-Location -Path $Root
 
-#region Create Symbolic Links
+#region Link Settings
 
-Write-Status -Message "Symlink configuration file . . ." -Step 1 -Total $Total
+Write-Status -Message "Symlink configuration files . . ." -Step 1 -Total $Total
 
 $Links = $Config | Select-Object -ExpandProperty $OperatingSystem
 $Links.PsObject.Properties.Value | Foreach-Object -ThrottleLimit 5 -Parallel {
@@ -34,9 +35,27 @@ $Links.PsObject.Properties.Value | Foreach-Object -ThrottleLimit 5 -Parallel {
 
 #endregion
 
+#region Link Custom Scripts
+
+Write-Status -Message "Symlink custom PowerShell scripts . . ." -Step 2 -Total $Total
+
+$Tools = Get-ChildItem -Path $([Path]::Combine($Root, "tools")) -Filter *.ps1
+$Tools | ForEach-Object -ThrottleLimit 5 -Parallel {
+    $Arguments = @{
+        Path = [System.IO.Path]::Combine($using:Scripts, $_.Name)
+        Value = $_.FullName
+        ItemType = "SymbolicLink"
+        Force = $true
+    }
+
+    New-Item @Arguments | Out-Null
+}
+
+#endregion
+
 #region Copy Assets
 
-Write-Status -Message "Copy icons to $Assets . . ." -Step 2 -Total $Total
+Write-Status -Message "Copy icons to $Assets . . ." -Step 3 -Total $Total
 
 New-Item -ItemType Directory -Path $Assets -Force | Out-Null
 Copy-Item -Path $([Path]::Combine($Root, "assets", "icons")) -Recurse -Destination $Assets -Force

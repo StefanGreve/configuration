1be3d0cafae2d26c24e1a80b948b5c07430fcfc3
StefanGreve <greve.stefan@outlook.jp>
2023-10-06 21:16:47 +0200

    Implement Test-Command and Install-WingetPackage

--- scripts/utils.ps1
+++ scripts/utils.ps1
@@ -10,6 +10,27 @@ function Get-OperatingSystem {
     }
 }
 
+function Test-Command {
+    [OutputType([bool])]
+    param(
+        [string] $Name
+    )
+
+    $PrevPreference = $ErrorActionPreference
+
+    try {
+        $ErrorActionPreference = "stop"
+        $_ = Get-Command $Name
+        return $true
+    }
+    catch {
+        return $false
+    }
+    finally {
+        $ErrorActionPreference = $PrevPreference
+    }
+}
+
 function Write-Status {
     param(
         [int] $Step,
@@ -22,5 +43,19 @@ function Write-Status {
     process {
         Write-Host "[$Step/$Total] " -NoNewline -ForegroundColor DarkGray
         Write-Host $Message
+    }
+}
+
+function Install-WingetPackage {
+    [OutputType([void])]
+    param(
+        [Parameter(Position = 0, Mandatory, ValueFromPipeline)]
+        [string[]] $Id
+    )
+
+    process {
+        foreach ($i in $Id) {
+            winget install --id $i --accept-package-agreements --accept-source-agreements --disable-interactivity
+        }
     }
 }

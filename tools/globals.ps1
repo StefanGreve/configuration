3bc58b14df7a85a14c03696c06f5790ec3227765
StefanGreve <greve.stefan@outlook.jp>
2023-10-20 22:28:24 +0200

    Define more global variables for commonly used paths

--- tools/globals.ps1
+++ tools/globals.ps1
@@ -1,6 +1,10 @@
 # General
 $global:DESKTOP = [Environment]::GetFolderPath("Desktop")
 $global:DOCUMENTS = [Environment]::GetFolderPath("MyDocuments")
+$global:PICTURES = [Environment]::GetFolderPath("MyPictures")
+$global:MUSIC = [Environment]::GetFolderPath("MyMusic")
+$global:VIDEOS = [Environment]::GetFolderPath("MyVideos")
+$global:DOWNLOADS = Join-Path -Path $HOME -ChildPath "Downloads"
 
 # Windows
 $global:HOSTS = "C:\Windows\System32\Drivers\etc\hosts"

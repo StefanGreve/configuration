466514c5fff67fa18dab248b3e0258fc645befac
StefanGreve <greve.stefan@outlook.jp>
2023-11-06 21:21:03 +0100

    Add first misc script: setup gcc on Windows

---
+++ scripts/misc/mingw64.ps1
@@ -0,0 +1,18 @@
+# Installs the GCC compiler on Windows
+
+if (![OperatingSystem]::IsWindows()) {
+    Write-Error "Unsupported Operating System" -ErrorAction Stop -Category DeviceError
+}
+
+$Archive = "MinGW64.zip"
+$Uri = "https://github.com/GorvGoyl/MinGW64/releases/download/v2.0/MinGW64.zip"
+
+Push-Location -Path $([Environment]::GetFolderPath("Desktop"))
+
+Invoke-WebRequest -Uri $Uri -OutFile $Archive
+Expand-Archive -Path $Archive -DestinationPath "C:/Program Files" -Force
+Remove-Item -Path $Archive
+
+[Environment]::SetEnvironmentVariable("PATH", "C:/Program Files/MinGW64/bin", [EnvironmentVariableTarget]::User)
+
+Pop-Location

2cf27b1d440860aefaeedb40678f1d369c97bb51
StefanGreve <greve.stefan@outlook.jp>
2024-12-08 17:45:01 +0100

    Restrict some global variables to Windows

--- scripts/globals.ps1
+++ scripts/globals.ps1
@@ -4,7 +4,9 @@ $global:DOCUMENTS = [Environment]::GetFolderPath("MyDocuments")
 $global:PICTURES = [Environment]::GetFolderPath("MyPictures")
 $global:MUSIC = [Environment]::GetFolderPath("MyMusic")
 $global:VIDEOS = [Environment]::GetFolderPath("MyVideos")
-$global:DOWNLOADS = Join-Path -Path $HOME -ChildPath "Downloads"
 
 # Windows
-$global:HOSTS = "C:\Windows\System32\Drivers\etc\hosts"
+if ($IsWindows) {
+    $global:HOSTS = "C:\Windows\System32\Drivers\etc\hosts"
+    $global:DOWNLOADS = Join-Path -Path $HOME -ChildPath "Downloads"
+}

44b58400699af4ac05d744651da7a1fdec73ccb4
StefanGreve <greve.stefan@outlook.jp>
2024-12-22 20:51:32 +0100

    Copy some settings (environment variables) for Windows

---
+++ scripts/windows/environment.ps1
@@ -0,0 +1,10 @@
+# dotnet
+$env:DOTNET_CLI_TELEMETRY_OPTOUT = 1
+
+# powershell
+$env:POWERSHELL_TELEMETRY_OPTOUT = 1
+$env:POWERSHELL_UPDATECHECK = "Stable"
+
+# powershell profile settings
+$env:PROFILE_ENABLE_BRANCH_USERNAME=1
+$env:PROFILE_LOAD_CUSTOM_SCRIPTS="~/Documents/Scripts"

28efaaec617572dd9ec78cce154e641de8965699
StefanGreve <greve.stefan@outlook.jp>
2026-08-05 21:29:10 +0200

    Add scheduled task for clocking out of work

---
+++ scripts/windows/ClockOutDaemon.ps1
@@ -0,0 +1,46 @@
+# ==============================================================================
+
+$Author = "Stefan Greve"
+$TaskName = "ClockOutDaemon"
+$Description = "Reminds you to clock out at 17:30 on weekdays."
+
+# ==============================================================================
+
+$ActionArgs = @{
+    Execute  = "pwsh.exe"
+    Argument = "-NoProfile -Command 'Stop-Work'"
+}
+$Action = New-ScheduledTaskAction @ActionArgs
+
+$TriggerArgs = @{
+    Weekly     = $true
+    DaysOfWeek = [DayOfWeek]::Monday..[DayOfWeek]::Friday
+    At         = "5:30PM"
+}
+$Trigger = New-ScheduledTaskTrigger @TriggerArgs
+
+$SettingsArgs = @{
+    ExecutionTimeLimit         = [TimeSpan]::FromMinutes(5)
+    AllowStartIfOnBatteries    = $true
+    DontStopIfGoingOnBatteries = $true
+    StartWhenAvailable         = $true
+}
+$Settings = New-ScheduledTaskSettingsSet @SettingsArgs
+
+$TaskArgs = @{
+    Action      = $Action
+    Trigger     = $Trigger
+    Settings    = $Settings
+    Description = $Description
+}
+$Task = New-ScheduledTask @TaskArgs
+$Task.Author = $Author
+$Task.Date = [DateTime]::Today.ToString("s")
+$Task.Source = $MyInvocation.MyCommand.Name
+
+$RegisterArgs = @{
+    TaskName    = $TaskName
+    InputObject = $Task
+    Force       = $true
+}
+Register-ScheduledTask @RegisterArgs
